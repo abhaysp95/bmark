b69b23f0e8bc14a4ca29669c7a370f2fb27d67ba
abhaysp95 <abhaysp95@protonmail.com>
2024-08-03 12:50:01 +0530

    feat: getting subcommand used and how to get related arguments

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
+use clap::ArgMatches;
+
 mod cli;
 
+fn perform_add_task(matches: &ArgMatches) {
+    let url = matches.get_one::<String>("url").expect("Passing URL is must");
+    println!("url passed: {}\n", url);
+}
+
+fn perform_list_task(_: &ArgMatches) {
+    println!("do list");
+}
+
 fn main() {
-    _ = cli::build_args();
+    let matches = cli::build_args();
+
+    if let Some(task) = matches.subcommand_matches("add") {
+        perform_add_task(task);
+    } else if let Some(task) = matches.subcommand_matches("list") {
+        perform_list_task(task);
+    }
 }
